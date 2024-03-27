26441a0912e0790f86d3c717001b8a1a7d280cd2
Timothy Keller <tkeller@bridge12.com>
2024-03-27 11:52:34 -0400

    Minor change to script

--- Software/Python/adf4351_serial_command.py
+++ Software/Python/adf4351_serial_command.py
@@ -156,7 +156,7 @@ def sweep_adc(points = 100, dwell_time = 1e-3):
     total_time = stop_time - start_time
     print('Total Sweep Time: %0.01fs'%total_time)
 
-def sweep_freq_adc(start_freq = 1000000,stop_freq = 2000000, points = 100, dwell_time = 1e-3):
+def sweep_freq_adc(start_freq = 1000000,stop_freq = 4000000, points = 100, dwell_time = 1e-3):
     freq_list = np.linspace(start_freq, stop_freq, points)
 #    freq_list = np.r_[start_freq:stop_freq:step]
 
@@ -184,7 +184,9 @@ atten(0)
 adc('tx')
 txamp(0)
 rxamp(0)
+power(0)
 sweep_freq_adc()
 atten(31)
-#ser.close()
+txamp(0)
+ser.close()
 show()
